01cd75d7b03258dc4463d116423f14c153b345df
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-26 23:32:43 +0330

    add AdminService class

---
+++ src/main/java/service/AdminService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Admin;
+import model.AdminRepository;
+import model.PostgresConnection;
+
+import java.sql.Connection;
+
+public class AdminService {
+    private Connection connection = PostgresConnection.getConnection();
+    private AdminRepository adminRepository = new AdminRepository();
+
+    public void singUp(Admin admin){
+        adminRepository.singUp(admin);
+    }
+
+    public Integer singIn(String username , String password){
+        return adminRepository.singIn(username , password);
+    }
+}
